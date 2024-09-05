933fb004c7f41def9ce4e9d05c6aa39bc98b653b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-05 21:35:06 +0330

    test(unit): increase coverage to 100%

--- tests/Unit/Attributes/OperationTest.php
+++ tests/Unit/Attributes/OperationTest.php
@@ -5,6 +5,39 @@ use Tests\Unit\Attributes\Stubs\SecuritySchemeFactoryInvalidStub;
 use Tests\Unit\Attributes\Stubs\SecuritySchemeFactoryStub;
 
 describe('Operation', function () {
+    it('can be instantiated with no parameters', function () {
+        $operation = new Operation();
+        expect($operation->id)->toBeNull();
+        expect($operation->tags)->toBeNull();
+        expect($operation->security)->toBeNull();
+        expect($operation->method)->toBeNull();
+        expect($operation->servers)->toBeNull();
+        expect($operation->summary)->toBeNull();
+        expect($operation->description)->toBeNull();
+        expect($operation->deprecated)->toBeNull();
+    });
+
+    it('can can be instantiated properly', function () {
+        $operation = new Operation(
+            id: 'id',
+            tags: 'tags',
+            security: SecuritySchemeFactoryStub::class,
+            method: 'method',
+            servers: 'servers',
+            summary: 'summary',
+            description: 'description',
+            deprecated: true,
+        );
+        expect($operation->id)->toBe('id');
+        expect($operation->tags)->toBe('tags');
+        expect($operation->security)->toBe(SecuritySchemeFactoryStub::class);
+        expect($operation->method)->toBe('method');
+        expect($operation->servers)->toBe('servers');
+        expect($operation->summary)->toBe('summary');
+        expect($operation->description)->toBe('description');
+        expect($operation->deprecated)->toBeTrue();
+    });
+
     it('can have no security', function () {
         $operation = new Operation();
         expect($operation->security)->toBeNull();
@@ -18,6 +51,16 @@ describe('Operation', function () {
     it('can be instantiated with an array of class FQN securities', function () {
         $operation = new Operation(security: [SecuritySchemeFactoryStub::class]);
         expect($operation->security)->toBe([SecuritySchemeFactoryStub::class]);
+    });
+
+    it('can be instantiated with an array of arrays of class FQN securities', function () {
+        $operation = new Operation(security: [[SecuritySchemeFactoryStub::class]]);
+        expect($operation->security)->toBe([[SecuritySchemeFactoryStub::class]]);
+    });
+
+    it('can be instantiated with a combination of class FQN and array of class FQN securities', function () {
+        $operation = new Operation(security: [SecuritySchemeFactoryStub::class, [SecuritySchemeFactoryStub::class]]);
+        expect($operation->security)->toBe([SecuritySchemeFactoryStub::class, [SecuritySchemeFactoryStub::class]]);
     });
 
     it('throws an exception when an invalid security is passed', function () {
