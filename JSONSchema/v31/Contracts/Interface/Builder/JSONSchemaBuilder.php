3f933eb16de9743e566b7ca1fee0bca8debc03d1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-06-01 21:44:24 +0330

    Delete JSONSchemaBuilder.php

--- JSONSchema/v31/Contracts/Interface/Builder/JSONSchemaBuilder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MohammadAlavi\ObjectOrientedJSONSchema\v31\Contracts\Interface\Builder;
-
-use MohammadAlavi\ObjectOrientedJSONSchema\Contracts\Interface\Builder\Builder;
-
-interface JSONSchemaBuilder extends Builder
-{
-}
