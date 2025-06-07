1fd19c263f687f8dde6e14b48635f18d17549e6b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-06-07 17:36:42 +0330

    Update TestReusableParameter.php

--- tests/Doubles/Stubs/Concerns/TestReusableParameter.php
+++ tests/Doubles/Stubs/Concerns/TestReusableParameter.php
@@ -2,20 +2,20 @@
 
 namespace Tests\Doubles\Stubs\Concerns;
 
-use MohammadAlavi\LaravelOpenApi\Contracts\Abstract\Factories\Components\ReusableParameterFactory as AbstractReusableParameterFactory;
+use MohammadAlavi\LaravelOpenApi\Contracts\Abstract\Factories\Components\ReusableParameterFactory;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Parameter\Fields\Common\Description;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Parameter\Fields\Common\Name;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Parameter\Parameter;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Parameter\SerializationRule\SchemaSerializedQuery;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Schema\Schema;
 
-class TestReusableParameter extends AbstractReusableParameterFactory
+class TestReusableParameter extends ReusableParameterFactory
 {
     public function build(): Parameter
     {
         return Parameter::query(
             Name::create('TestReusableParameter'),
-            SchemaSerializedQuery::create(Schema::create()),
+            SchemaSerializedQuery::create(Schema::string()),
         )->description(Description::create('ReusableParameterStub description'))
             ->required();
     }
