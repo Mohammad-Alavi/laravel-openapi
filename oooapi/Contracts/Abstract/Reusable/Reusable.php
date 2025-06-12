fa57a59d80f862e9a74d6504cfbbed465f5e3ba5
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-06-12 22:12:01 +0330

    Update Reusable.php

--- oooapi/Contracts/Abstract/Reusable/Reusable.php
+++ oooapi/Contracts/Abstract/Reusable/Reusable.php
@@ -4,6 +4,7 @@ namespace MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Reusable;
 
 use MohammadAlavi\ObjectOrientedJSONSchema\Draft202012\Contracts\Descriptor\Descriptor;
 use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Interface\Reusable as ReusableContract;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Reference\Fields\Ref;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Reference\Reference;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Schema\Contracts\OpenAPISchema;
 use Webmozart\Assert\Assert;
@@ -12,42 +13,42 @@ abstract class Reusable implements ReusableContract
 {
     final public static function new(): Descriptor
     {
-        return OpenAPISchema::withoutSchema()->ref(static::ref());
-    }
-
-    final public static function create(): static
-    {
-        return new static();
+        return OpenAPISchema::withoutSchema()->ref(self::reference()->ref());
     }
 
     /**
      * The reference to the reusable component.
      */
-    abstract public static function ref(): Reference|string;
-
-    final protected static function path(): string
+    final public static function reference(): Reference
     {
-        return static::basePath() .
-            static::componentPath() . '/' .
-            static::key();
+        $ref = self::baseNamespace() . static::componentNamespace() . '/' . static::name();
+
+        return Reference::create(
+            Ref::create($ref),
+        );
     }
 
-    private static function basePath(): string
+    private static function baseNamespace(): string
     {
         return '#/components';
     }
 
-    abstract protected static function componentPath(): string;
+    abstract protected static function componentNamespace(): string;
 
     /**
      * The key of the reusable component that is used to reference it in other places.
      */
-    public static function key(): string
+    public static function name(): string
     {
         $key = class_basename(static::class);
 
         Assert::regex($key, '/^[a-zA-Z0-9.\-_]+$/');
 
         return $key;
+    }
+
+    final public static function create(): static
+    {
+        return new static();
     }
 }
