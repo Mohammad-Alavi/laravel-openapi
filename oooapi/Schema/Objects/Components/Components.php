b00c1795c013f24f0a0b24334cf9332f1ffda48b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-06-27 18:50:56 +0330

    Update Components.php

--- oooapi/Schema/Objects/Components/Components.php
+++ oooapi/Schema/Objects/Components/Components.php
@@ -9,6 +9,11 @@ use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\Components\
 use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\Components\SchemaFactory;
 use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\Components\SecuritySchemeFactory;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Callback\Callback;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Parameter\Parameter;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\RequestBody\RequestBody;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Response\Response;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Schema\Schema;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Security\SecurityScheme\SecurityScheme;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Support\ExtensibleObject;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Support\SharedFields\Examples\ExampleEntry;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Support\SharedFields\Examples\Examples;
@@ -20,45 +25,59 @@ use MohammadAlavi\ObjectOrientedOpenAPI\Support\Arr;
 
 final class Components extends ExtensibleObject
 {
-    /** @var SchemaFactory[]|null */
+    /** @var Schema[]|null */
     private array|null $schemas = null;
-    /** @var ResponseFactory[]|null */
+
+    /** @var Response[]|null */
     private array|null $responses = null;
-    /** @var ParameterFactory[]|null */
+
+    /** @var Parameter[]|null */
     private array|null $parameters = null;
+
     private Examples|null $examples = null;
-    /** @var RequestBodyFactory[]|null */
+
+    /** @var RequestBody[]|null */
     private array|null $requestBodies = null;
+
     private Headers|null $headers = null;
-    /** @var SecuritySchemeFactory[]|null */
+
+    /** @var SecurityScheme[]|null */
     private array|null $securitySchemes = null;
+
     private Links|null $links = null;
+
     /** @var Callback[]|null */
     private array|null $callbacks = null;
 
-    public function schemas(SchemaFactory ...$schema): self
+    public function schemas(SchemaFactory ...$schemaFactory): self
     {
         $clone = clone $this;
 
-        $clone->schemas = [] !== $schema ? $schema : null;
+        foreach ($schemaFactory as $factory) {
+            $clone->schemas[$factory::name()] = $factory->component();
+        }
 
         return $clone;
     }
 
-    public function responses(ResponseFactory ...$reusableResponseFactory): self
+    public function responses(ResponseFactory ...$responseFactory): self
     {
         $clone = clone $this;
 
-        $clone->responses = $reusableResponseFactory;
+        foreach ($responseFactory as $factory) {
+            $clone->responses[$factory::name()] = $factory->component();
+        }
 
         return $clone;
     }
 
-    public function parameters(ParameterFactory ...$reusableParameterFactory): self
+    public function parameters(ParameterFactory ...$parameterFactory): self
     {
         $clone = clone $this;
 
-        $clone->parameters = [] !== $reusableParameterFactory ? $reusableParameterFactory : null;
+        foreach ($parameterFactory as $factory) {
+            $clone->parameters[$factory::name()] = $factory->component();
+        }
 
         return $clone;
     }
@@ -77,11 +96,13 @@ final class Components extends ExtensibleObject
         return new self();
     }
 
-    public function requestBodies(RequestBodyFactory ...$reusableRequestBodyFactory): self
+    public function requestBodies(RequestBodyFactory ...$requestBodyFactory): self
     {
         $clone = clone $this;
 
-        $clone->requestBodies = [] !== $reusableRequestBodyFactory ? $reusableRequestBodyFactory : null;
+        foreach ($requestBodyFactory as $factory) {
+            $clone->requestBodies[$factory::name()] = $factory->component();
+        }
 
         return $clone;
     }
@@ -99,7 +120,9 @@ final class Components extends ExtensibleObject
     {
         $clone = clone $this;
 
-        $clone->securitySchemes = [] !== $securitySchemeFactory ? $securitySchemeFactory : null;
+        foreach ($securitySchemeFactory as $factory) {
+            $clone->securitySchemes[$factory::name()] = $factory->component();
+        }
 
         return $clone;
     }
@@ -126,39 +149,14 @@ final class Components extends ExtensibleObject
 
     protected function toArray(): array
     {
-        $schemas = [];
-        foreach ($this->schemas ?? [] as $schema) {
-            $schemas[$schema::name()] = $schema->component();
-        }
-
-        $responses = [];
-        foreach ($this->responses ?? [] as $response) {
-            $responses[$response::name()] = $response->component();
-        }
-
-        $parameters = [];
-        foreach ($this->parameters ?? [] as $parameter) {
-            $parameters[$parameter::name()] = $parameter->component();
-        }
-
-        $requestBodies = [];
-        foreach ($this->requestBodies ?? [] as $requestBody) {
-            $requestBodies[$requestBody::name()] = $requestBody->component();
-        }
-
-        $securitySchemes = [];
-        foreach ($this->securitySchemes ?? [] as $securityScheme) {
-            $securitySchemes[$securityScheme::name()] = $securityScheme->component();
-        }
-
         return Arr::filter([
-            'schemas' => when(blank($schemas), null, $schemas),
-            'responses' => when(blank($responses), null, $responses),
-            'parameters' => when(blank($parameters), null, $parameters),
+            'schemas' => $this->schemas,
+            'responses' => $this->responses,
+            'parameters' => $this->parameters,
             'examples' => $this->examples,
-            'requestBodies' => when(blank($requestBodies), null, $requestBodies),
+            'requestBodies' => $this->requestBodies,
             'headers' => $this->headers,
-            'securitySchemes' => when(blank($securitySchemes), null, $securitySchemes),
+            'securitySchemes' => $this->securitySchemes,
             'links' => $this->links,
             'callbacks' => $this->callbacks,
             // TODO: add extensions
