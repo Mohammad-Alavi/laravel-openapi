61df483a4e530d7fe8cf814e61712bca0a0c321e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-06-27 21:06:07 +0330

    Update ComponentsBuilderTest.php

--- tests/src/Unit/Builders/Components/ComponentsBuilderTest.php
+++ tests/src/Unit/Builders/Components/ComponentsBuilderTest.php
@@ -3,40 +3,42 @@
 use Illuminate\Support\Facades\Config;
 use MohammadAlavi\LaravelOpenApi\Builders\Components\ComponentsBuilder;
 use MohammadAlavi\LaravelOpenApi\Generator;
+use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\Components\SecuritySchemeFactory;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Components\Components;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Security\SecurityScheme\Schemes\Http;
+use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Security\SecurityScheme\SecurityScheme;
 use Pest\Expectation;
 
 describe(class_basename(ComponentsBuilder::class), function (): void {
     beforeEach(function (): void {
         Config::set('openapi', [
-            //            'collections' => [
-            //                'test' => [
-            //                    'security' => [
-            //                        (new class () extends SecuritySchemeFactory {
-            //                            public function build(): SecurityScheme
-            //                            {
-            //                                return SecurityScheme::create('test')
-            //                                    ->type('http')
-            //                                    ->scheme('bearer');
-            //                            }
-            //                        })::class,
-            //                    ],
-            //                ],
-            //            ],
+            'collections' => [
+                'test' => [
+                    'security' => [
+                        (new class extends SecuritySchemeFactory {
+                            public function component(): SecurityScheme
+                            {
+                                return SecurityScheme::http(Http::bearer());
+                            }
+                        })::class,
+                    ],
+                ],
+            ],
             'locations' => [
                 'callbacks' => [
-                    __DIR__ . '/../../Doubles/Stubs/Collectors/Components/Callback',
+                    __DIR__ . '/../../../Support/Doubles/Stubs/Collectors/Components/Callback',
                 ],
                 'request_bodies' => [
-                    __DIR__ . '/../../Doubles/Stubs/Collectors/Components/RequestBody',
+                    __DIR__ . '/../../../Support/Doubles/Stubs/Collectors/Components/RequestBody',
                 ],
                 'responses' => [
-                    __DIR__ . '/../../Doubles/Stubs/Collectors/Components/Response',
+                    __DIR__ . '/../../../Support/Doubles/Stubs/Collectors/Components/Response',
                 ],
                 'schemas' => [
-                    __DIR__ . '/../../Doubles/Stubs/Collectors/Components/Schema',
+                    __DIR__ . '/../../../Support/Doubles/Stubs/Collectors/Components/Schema',
                 ],
                 'security' => [
-                    __DIR__ . '/../../Doubles/Stubs/Collectors/Components/SecurityScheme',
+                    __DIR__ . '/../../../Support/Doubles/Stubs/Collectors/Components/SecurityScheme',
                 ],
             ],
         ]);
@@ -45,11 +47,14 @@ describe(class_basename(ComponentsBuilder::class), function (): void {
     it('can collect components', function (string|null $collection, array|null $expectation): void {
         $componentsBuilder = app(ComponentsBuilder::class);
 
+        /** @var Components|null $result */
         $result = $componentsBuilder->build($collection);
 
         expect($result?->asArray())->unless(
             is_null($result),
-            fn (Expectation $xp): Expectation => $xp->toEqual($expectation),
+            function (Expectation $xp) use ($expectation): Expectation {
+                return $xp->toEqual($expectation);
+            },
         );
     })->with(
         [
