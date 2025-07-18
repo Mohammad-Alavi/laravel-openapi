e64b70a7fc7911eb5fa24a70dc1712076f54fe70
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-07-18 15:38:33 +0330

    Implement ShouldBeReferenced in SecuritySchemeFactory

--- oooapi/Contracts/Abstract/Factories/Components/SecuritySchemeFactory.php
+++ oooapi/Contracts/Abstract/Factories/Components/SecuritySchemeFactory.php
@@ -3,9 +3,10 @@
 namespace MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\Components;
 
 use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Abstract\Factories\ReusableComponent;
+use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Interface\ShouldBeReferenced;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\Objects\Security\SecurityScheme\SecurityScheme;
 
-abstract class SecuritySchemeFactory extends ReusableComponent
+abstract class SecuritySchemeFactory extends ReusableComponent implements ShouldBeReferenced
 {
     final protected static function componentNamespace(): string
     {
