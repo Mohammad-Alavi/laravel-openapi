75ea20ed9df570c5d59575f2fb0ca49cd02f5ee7
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-07-22 12:02:08 +0330

    Update CreateUserRequest.php

--- workbench/app/Http/Requests/CreateUserRequest.php
+++ workbench/app/Http/Requests/CreateUserRequest.php
@@ -3,7 +3,6 @@
 namespace Workbench\App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use LaravelRulesToSchema\Facades\LaravelRulesToSchema;
 
 class CreateUserRequest extends FormRequest
 {
