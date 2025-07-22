064f3b3ec4da3bb2147c6398fd6d127bb22b3d17
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-07-22 12:00:44 +0330

    Update CreateUserRequest.php

--- workbench/app/Http/Requests/CreateUserRequest.php
+++ workbench/app/Http/Requests/CreateUserRequest.php
@@ -23,9 +23,6 @@ class CreateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // TODO: Handle mixed validation rules of string and array types
-            // The underlying package ,"LaravelRulesToSchema::parse()", cannot handle it yet.
-            // 'name' => ['required_without:email', 'string|max:255'],
             'name' => ['required_without:email', 'string', 'max:255'],
             'email' => 'required_without:name|email|max:255',
             'password' => 'string|min:8|confirmed',
